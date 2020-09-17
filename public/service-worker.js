d730c25053a01caf1ccabba3e1d78ad89cdd1f48
egibbs834 <edwardgibbons34@gmail.com>
2020-09-16 22:47:41 -0400

    working offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,11 +4,11 @@ const FILES_TO_CACHE = [
   "/index.js",
   "/style.css",
   "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png",
+  "/icons/icon-512x512.png"
 ];
 
-const CACHE_NAME = "static-cache-v13";
-const DATA_CACHE_NAME = "data-cache-v8";
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 
 console.log("if you see me service worker exists");
 
@@ -44,14 +44,36 @@ self.addEventListener("activate", function (evt) {
 
 // enable service-worker to intercept network requests
 self.addEventListener("fetch", function (evt) {
-  // code to handle requests goes here
-});
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
 
-// serve static files from cache
-evt.respondWith(
-  caches.open(CACHE_NAME).then((cache) => {
-    return cache.match(evt.request).then((response) => {
-      return response || fetch(evt.request);
-    });
-  })
-);
+              return response;
+            })
+            .catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+
+    return;
+  }
+
+  evt.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(evt.request).then((response) => {
+        return response || fetch(evt.request);
+      });
+    })
+  );
+});
