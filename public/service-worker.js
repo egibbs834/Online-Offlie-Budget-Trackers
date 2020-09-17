c16bc69d9e9cd47ebdc816870c67d3f91a8ce4d8
egibbs834 <edwardgibbons34@gmail.com>
2020-09-16 21:48:10 -0400

    enabled service-worker to intercept network request (starting code) and serve static files from cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,25 +1,57 @@
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/index.js",
-    "/style.css",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
+  "/",
+  "/index.html",
+  "/index.js",
+  "/style.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
 ];
 
-    const CACHE_NAME = 'static-cache-v13';
-    const DATA_CACHE_NAME = 'data-cache-v8';
+const CACHE_NAME = "static-cache-v13";
+const DATA_CACHE_NAME = "data-cache-v8";
 
-console.log("if you see me service worker exists")
+console.log("if you see me service worker exists");
 
 // installs and regiesters service-worker
-self.addEventListener('install', function(evt) {
-    evt.waitUntil(
-      caches.open(CACHE_NAME).then(cache => {
-        console.log("Your files were pre-cached successfully!");
-        return cache.addAll(FILES_TO_CACHE);
-      })
-    );
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 
-    self.skipWaiting();
-  });
+  self.skipWaiting();
+});
+
+//   activate service-worker and remove old data from the cache
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// enable service-worker to intercept network requests
+self.addEventListener("fetch", function (evt) {
+  // code to handle requests goes here
+});
+
+// serve static files from cache
+evt.respondWith(
+  caches.open(CACHE_NAME).then((cache) => {
+    return cache.match(evt.request).then((response) => {
+      return response || fetch(evt.request);
+    });
+  })
+);
